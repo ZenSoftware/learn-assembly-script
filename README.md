100a260a35f28e9d328d4f98da218f37cc1d86b6
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-10 07:07:07 -0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Clean slate for experimenting with [AssemblyScript](https://www.assemblyscript.org/)
---
+++ assembly/index.ts
@@ -0,0 +1,4 @@
+export function add(a: i32, b: i32): i32 {
+  console.log(`Sample ${isInteger(123)}`);
+  return a + b;
+}
